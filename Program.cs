b7ed57f89710e6b06ec990ebbab91e3811109e62
AlakbarovAyxan <136125673+AlakbarovAyxan@users.noreply.github.com>
2023-07-08 13:22:59 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿#region kalkulyator
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double reqem1 = Convert.ToInt32(Console.ReadLine());
+        string isare = Console.ReadLine();
+        double reqem2 = Convert.ToInt32(Console.ReadLine());
+        double netice = 0;
+        
+        //Console.Write(isare);
+        //Console.WriteLine(reqem2);
+        //Console.Write("=");
+        for (int i = 0; i < 1; i++)
+        {
+            if (isare[0] == '+')
+            {
+                netice = reqem1 + reqem2;
+            }
+            else if (isare[0] == '-')
+            {
+                netice = reqem1 - reqem2;
+            }
+            else if (isare[0] == '*')
+            {
+                netice = reqem1 * reqem2;
+            }
+            else if (isare[0] == '/')
+            {
+                netice = reqem1 / reqem2;
+            }
+            else
+            {
+                Console.WriteLine("duzgun isare secmediz");
+                return;
+            }
+        }
+        Console.Write(reqem1+isare+reqem2+"=");
+        Console.Write(netice);
+    }
+}
+#endregion
